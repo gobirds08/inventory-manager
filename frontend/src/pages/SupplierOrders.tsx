b9522dbdf18e5c8fa90f52a265c5e600db2d0c1d
Brendan Kenney <brendankenney3@gmail.com>
2025-01-28 15:00:52 -0500

    now use useEffect for orders and display in table

--- frontend/src/pages/SupplierOrders.tsx
+++ frontend/src/pages/SupplierOrders.tsx
@@ -1,9 +1,22 @@
 import { SupplierOrder } from "../models/SupplierOrders";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { fetchSupplierOrders } from "../utilities/FetchData";
 import SupplierOrderView from "../components/supplier_order_view/SupplierOrderView";
 
 function SupplierOrders() {
 	const [orders, setOrders] = useState<SupplierOrder[]>([]);
+
+	useEffect(() => {
+		const fetchOrders = async () => {
+			const orders: SupplierOrder[] = await fetchSupplierOrders();
+			const updatedOrders = orders.map((order) => ({
+				...order,
+				order_date: new Date(order.order_date),
+			}));
+			setOrders(updatedOrders);
+		};
+		fetchOrders();
+	}, []);
 
 	return (
 		<>
@@ -21,7 +34,7 @@ function SupplierOrders() {
 						<tr key={order.supplier_order_id}>
 							<td>{order.supplier_order_id}</td>
 							<td>{order.supplier_id}</td>
-							<td>{order.order_date.toISOString()}</td>
+							<td>{order.order_date.toUTCString()}</td>
 						</tr>
 					))}
 					{/* maybe create some sort of delete or sort buttons */}
