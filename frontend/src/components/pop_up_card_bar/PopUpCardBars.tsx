95e125522eb95b1a330c673392260c1b36c2b5be
Brendan Kenney <brendankenney3@gmail.com>
2025-01-30 20:32:54 -0500

    ProductView for products shown

--- frontend/src/components/pop_up_card_bar/PopUpCardBars.tsx
+++ frontend/src/components/pop_up_card_bar/PopUpCardBars.tsx
@@ -2,6 +2,7 @@ import { SupplierOrder } from "../../models/SupplierOrderDetails";
 import { Product } from "../../models/Product";
 import { useEffect, useState } from "react";
 import { SupplierOrderDetail } from "../../models/SupplierOrderDetails";
+import ProductView from "../product_view/ProductView";
 
 interface PopUpCardBarProps {
 	supplier_orders: SupplierOrderDetail[];
@@ -9,6 +10,22 @@ interface PopUpCardBarProps {
 
 function PopUpCardBar({ supplier_orders }: PopUpCardBarProps) {
 	const [products, setProducts] = useState<Product[]>([]);
+	const [firstProductIndex, setFirstProductIndex] = useState<number>(0);
+	const numProductsToShow = 5;
+
+	function handleArrowClick(left: boolean) {
+		if (left) {
+			if (firstProductIndex === 0) {
+				return;
+			}
+			setFirstProductIndex(firstProductIndex + 1);
+		} else {
+			if (firstProductIndex === products.length - 1) {
+				return;
+			}
+			setFirstProductIndex(firstProductIndex - 1);
+		}
+	}
 
 	useEffect(() => {
 		let temp_products: Product[] = [];
@@ -30,9 +47,17 @@ function PopUpCardBar({ supplier_orders }: PopUpCardBarProps) {
 
 	return (
 		<div className="container">
-			<button className="arrow-left">&#8592; Left</button>
-			<></>
-			<button className="arrow-right">Right &#8594;</button>
+			<button className="arrow-left" onClick={() => handleArrowClick(true)}>
+				&#8592; Left
+			</button>
+			{products
+				.slice(firstProductIndex, firstProductIndex + numProductsToShow)
+				.map((product) => (
+					<ProductView key={product.product_id} product={product} />
+				))}
+			<button className="arrow-right" onClick={() => handleArrowClick(false)}>
+				Right &#8594;
+			</button>
 		</div>
 	);
 }
