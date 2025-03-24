95fbde1c51cae520b0025993e4e04ae97c36dcf5
Brendan Kenney <brendankenney3@gmail.com>
2025-03-24 15:39:10 -0400

    progress on login css

--- frontend/src/components/login/login.tsx
+++ frontend/src/components/login/login.tsx
@@ -7,14 +7,14 @@ function Login() {
 	}
 
 	return (
-		<div>
+		<div className="login-container">
 			<h1>Login</h1>
-			<form onSubmit={handleSubmit}>
-				<div>
+			<form onSubmit={handleSubmit} className="">
+				<div className="form-group">
 					<label htmlFor="email">Email</label>
 					<input type="email" id="email" name="email" />
 				</div>
-				<div>
+				<div className="form-group">
 					<label htmlFor="password">Password</label>
 					<input type="password" id="password" name="password" />
 				</div>
